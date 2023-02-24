b708a7311e0c3caf56d7ed8846f4a6944a5a600c
Alvaro-LGPA <alvarolaura@gmail.com>
2023-02-24 11:43:09 +0100

    Start search feature implementation

--- src/app/store.js
+++ src/app/store.js
@@ -6,13 +6,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import { cartReducer } from '../features/cart/cartSlice';
 import { currencyFilterReducer } from '../features/currencyFilter/currencyFilterSlice';
 import { inventoryReducer } from '../features/inventory/inventorySlice'
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice'
 // Create and export the store here.
 
 
 const reducers = {
     cart: cartReducer,
     currency: currencyFilterReducer,
-    inventory: inventoryReducer
+    inventory: inventoryReducer,
+    search: searchTermReducer
 }
 
 // export const store = configureStore(combineReducers(reducers));
